fe46da7cb2e10eed5b6124c461279bcbb6702106
Mitsuki Tanaka <mitsuki.tanaka@esm.co.jp>
2019-05-10 14:59:36 +0900

    第1章の問題

---
+++ chapter1/Aisatsu.java
@@ -0,0 +1,10 @@
+package chapter1;
+
+/**
+ * Q1-4 画面に挨拶を表示するプログラム
+ */
+public class Aisatsu {
+ public static void main(String[] args) {
+  System.out.println("おはよう。\nこんにちは。\nこんばんは。");
+  }
+ }
---
+++ chapter1/Kantan.java
@@ -0,0 +1,12 @@
+package chapter1;
+
+/**
+ * Q1-3 意外に"Java"って簡単だ と表示するプログラム
+ */
+public class Kantan {
+    public static void main(String[] args) {
+        System.out.println("意外に\"Java\"って簡単だ。");
+        System.out.println("意外に\"Java\"って簡単だ。");
+        System.out.println("意外に\"Java\"って簡単だ。");
+    }
+}
