a168752fc1fce97d07268961f65ef64a1fee0baa
Mitsuki Tanaka <mitsuki.tanaka@esm.co.jp>
2019-05-13 15:26:02 +0900

    インデントの修正

--- chapter2/Daikei.java
+++ chapter2/Daikei.java
@@ -4,7 +4,7 @@ package chapter2;
  * Q2-3 上辺2センチ、底辺3センチ、高さ4センチの台形の面積を表示
  */
 public class Daikei {
-	public static void main(String[] args) {
-		System.out.println("面積＝" + (2 + 3) * 4 / 2);
-	}
+    public static void main(String[] args) {
+        System.out.println("面積＝" + (2 + 3) * 4 / 2);
+    }
 }
