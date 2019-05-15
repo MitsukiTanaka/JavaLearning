ff97740341365ba0eaf64c4c53b903bad5904de6
Mitsuki Tanaka <mitsuki.tanaka@esm.co.jp>
2019-05-15 10:18:44 +0900

    不要な行の削除と、変数名の変更

--- chapter3/Unicode.java
+++ chapter3/Unicode.java
@@ -11,29 +11,24 @@ public class Unicode {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         try {
             String line = reader.readLine();
-            char c0 = line.charAt(0);
-            int code0 = (int) c0;
-            System.out.println("'" + c0 + "'の文字コードは" + (int) c0 + "です");
+            char c;
+            c = line.charAt(0);
+            System.out.println("'" + c + "'の文字コードは" + (int) c + "です");
 
-            char c1 = line.charAt(1);
-            int code1 = (int) c1;
-            System.out.println("'" + c1 + "'の文字コードは" + (int) c1 + "です");
+            c = line.charAt(1);
+            System.out.println("'" + c + "'の文字コードは" + (int) c + "です");
 
-            char c2 = line.charAt(2);
-            int code2 = (int) c2;
-            System.out.println("'" + c2 + "'の文字コードは" + (int) c2 + "です");
+            c = line.charAt(2);
+            System.out.println("'" + c + "'の文字コードは" + (int) c + "です");
 
-            char c3 = line.charAt(3);
-            int code3 = (int) c3;
-            System.out.println("'" + c3 + "'の文字コードは" + (int) c3 + "です");
+            c = line.charAt(3);
+            System.out.println("'" + c + "'の文字コードは" + (int) c + "です");
 
-            char c4 = line.charAt(4);
-            int code4 = (int) c4;
-            System.out.println("'" + c4 + "'の文字コードは" + (int) c4 + "です");
+            c = line.charAt(4);
+            System.out.println("'" + c + "'の文字コードは" + (int) c + "です");
 
-            char c5 = line.charAt(5);
-            int code5 = (int) c5;
-            System.out.println("'" + c5 + "'の文字コードは" + (int) c5 + "です");
+            c = line.charAt(5);
+            System.out.println("'" + c + "'の文字コードは" + (int) c + "です");
         } catch (IOException e) {
             System.out.println(e);
         }
