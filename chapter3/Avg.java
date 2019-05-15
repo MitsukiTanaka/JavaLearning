b050064edb7137cc703a7c1da0bde514a0874a60
Mitsuki Tanaka <mitsuki.tanaka@esm.co.jp>
2019-05-15 14:47:43 +0900

    入力された年齢を設定する変数の統一と、処理内容のコメントの追加

--- chapter3/Avg.java
+++ chapter3/Avg.java
@@ -7,22 +7,26 @@ import java.io.*;
  */
 public class Avg {
     public static void main(String[] args) {
-        System.out.println("1人目：名前を入力してください");
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         try {
+            /* 1人目の処理 */
+            System.out.println("1人目：名前を入力してください");
             String name1 = reader.readLine();
             System.out.println(name1 + "さんこんにちは");
             System.out.println("年齢を入力してください");
-            String tosi1 = reader.readLine();
-            int age1 = Integer.parseInt(tosi1);
+            String tosi = reader.readLine();
+            int age1 = Integer.parseInt(tosi);
 
+            /* 2人目の処理 */
             System.out.println("2人目：名前を入力してください");
             String name2 = reader.readLine();
             System.out.println(name2 + "さんこんにちは");
             System.out.println("年齢を入力してください");
-            String tosi2 = reader.readLine();
-            int age2 = Integer.parseInt(tosi2);
+            tosi = reader.readLine();
+            int age2 = Integer.parseInt(tosi);
 
+            /* 2人の平均年齢を表示 */
             System.out.println(name1 + "さんと" + name2 + "さんの年齢の平均は" + (age1 + age2) / 2 + "歳です");
 
         } catch (IOException e) {
