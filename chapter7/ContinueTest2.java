027fc59221ee7e29af437fbb71f89b35f42f8d33
Mitsuki Tanaka <mitsuki.tanaka@esm.co.jp>
2019-05-27 09:23:05 +0900

    問題7-4

---
+++ chapter7/ContinueTest2.java
@@ -0,0 +1,46 @@
+package chapter7;
+
+import java.io.*;
+
+/**
+ * Q7-4 下記は何をするプログラムか。<br>
+ *      また、Continue文を使わない形式に書き換える。<br>
+ * <pre>{@code
+ *     public class ContinueTest {
+ *     public static void main(String[] args) {
+ *         BufferedReader reader = new BufferedReader((new InputStreamReader(System.in)));
+ *         try {
+ *             String line;
+ *             while ((line = reader.readLine()) !=null) {
+ *                 if (!line.endsWith("{") && !line.endsWith("}")) {
+ *                     continue;
+ *                 }
+ *                 System.out.print(line);
+ *             }
+ *         } catch (IOException e) {
+ *             System.out.println(e);
+ *         }
+ *     }
+ * }
+ * }</pre>
+ */
+public class ContinueTest2 {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader((new InputStreamReader(System.in)));
+        try {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.endsWith("{") || line.endsWith("}")) {
+                    System.out.print(line);
+                } else {
+                    /* 処理なし */
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+}
+/**
+ * 入力した文字列の末尾に "{" もしくは "}" がある場合、出力されるプログラム
+ */
