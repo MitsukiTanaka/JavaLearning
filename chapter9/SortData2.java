b973bdea23144c966f1c02bc50bf2e91ed7ae958
Mitsuki Tanaka <mitsuki.tanaka@esm.co.jp>
2019-05-29 16:00:44 +0900

    問題9-8

---
+++ chapter9/SortData2.java
@@ -0,0 +1,31 @@
+package chapter9;
+
+/**
+ * Q9-8 ソートプログラム
+ */
+public class SortData2 {
+    public static void main(String[] args) {
+        int[] data = {31,41,59,26,53,58,97,93,23,84};
+        System.out.println("並べ替える前");
+        for (int i = 0; i < data.length; i++) {
+            System.out.print(data[i] + " ");
+        }
+        System.out.println("");
+        for (int i = 0; i < data.length; i++) {
+            for (int j = i + 1; j < data.length; j++) {
+                if (data[i] > data[j]) {
+                    /* 交換 */
+                    int number;
+                    number = data[i];
+                    data[i] = data[j];
+                    data[j] = number;
+                }
+            }
+        }
+        System.out.println("並べ替えた後");
+        for (int i = 0; i < data.length; i++) {
+            System.out.print(data[i] + " ");
+        }
+        System.out.println("");
+    }
+}
