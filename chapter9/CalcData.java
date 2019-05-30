283907f886276017c665fb085dba0d06d67f9c16
Mitsuki Tanaka <mitsuki.tanaka@esm.co.jp>
2019-05-30 11:02:15 +0900

    コメントの追加

--- chapter9/CalcData.java
+++ chapter9/CalcData.java
@@ -25,7 +25,7 @@ package chapter9;
 public class CalcData {
     public static void main(String[] args) {
         double[] data;
-        data = new double[6];
+        data = new double[6];   /* 配列の長さを 6 に修正 */
         data[0] = 3.1;
         data[1] = 4.1;
         data[2] = 5.9;
@@ -33,7 +33,7 @@ public class CalcData {
         data[4] = 5.3;
         data[5] = 9.7;
         double sum = 0.0;
-        for (int i = 0; i < data.length; i++) {
+        for (int i = 0; i < data.length; i++) {   /* for文の条件式 i <= data.length を i < data.length に修正*/
             sum += data[i];
         }
         System.out.println("sum = " + sum);
