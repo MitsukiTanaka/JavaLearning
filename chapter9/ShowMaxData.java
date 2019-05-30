a1219402e6f2de753eaaf16511407af865d16bb4
Mitsuki Tanaka <mitsuki.tanaka@esm.co.jp>
2019-05-30 11:04:21 +0900

    不要な代入式の削除

--- chapter9/ShowMaxData.java
+++ chapter9/ShowMaxData.java
@@ -8,9 +8,8 @@ public class ShowMaxData {
         int[] data = {31, 41, 59, 26, 53, 58, 97, 93, 53, 84};
         int max_data = data[0];
         for (int i = 0; i < data.length; i++) {
-            int n = data[i];
-            if (max_data < n) {
-                max_data = n;
+            if (max_data < data[i]) {
+                max_data = data[i];
             }
         }
         System.out.println("最大値は" + max_data + "です。");
