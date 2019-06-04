3aa7da74c8dce111a484e9983d4d7e6e664f40b9
Mitsuki Tanaka <mitsuki.tanaka@esm.co.jp>
2019-06-04 15:18:07 +0900

    問題11-5

---
+++ chapter11/GamePlayer.java
@@ -0,0 +1,27 @@
+package chapter11;
+
+/**
+ * 問題11-5
+ */
+public class GamePlayer {
+    public String playername;   /* playername をインスタンスフィールドにする */
+
+    public GamePlayer(String name) {
+        playername = name;
+    }
+
+    @Override
+    public String toString() {
+        return "[player:" + playername + "]";
+    }
+
+    public static void main(String[] args) {
+        GamePlayer[] player = new GamePlayer[3];
+        player[0] = new GamePlayer("Mad Hatter");
+        player[1] = new GamePlayer("March Hare");
+        player[2] = new GamePlayer("Alice");
+        for (int i = 0; i < player.length; i++) {
+            System.out.println(player[i]);
+        }
+    }
+}
