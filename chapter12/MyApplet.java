b55d5657f374fa9c7fbaa1ded53a7e9818f7803f
Mitsuki Tanaka <mitsuki.tanaka@esm.co.jp>
2019-06-06 17:45:50 +0900

    問題12-2

---
+++ chapter12/MyApplet.java
@@ -0,0 +1,17 @@
+package chapter12;
+
+import java.applet.*;
+
+/**
+ * 問題12－2　MyAppletをAppletクラスのサブクラスにする<br>
+ * <pre>{@code
+ * import java.applet.*;
+ *
+ * public class MyApplet{
+ *     ...
+ * }
+ * }</pre>
+ */
+public class MyApplet extends Applet {
+
+}
