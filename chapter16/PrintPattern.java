05d5d1f2e0f5500f23de735b62531e8ae16111b4
Mitsuki Tanaka <mitsuki.tanaka@esm.co.jp>
2019-06-21 09:29:09 +0900

    問題16-6

---
+++ chapter16/PrintPattern.java
@@ -0,0 +1,41 @@
+package chapter16;
+
+/**
+ * 問題16-6<br>
+ * 約3秒ごとに"***"を10回表示するスレッドと、約5秒ごとに"====="を10回表示するスレッドを、
+ * それぞれひとつずつ動かすクラスをThreadクラスの拡張クラスとして作る。
+ */
+public class PrintPattern {
+    public static void main(String[] args) {
+        new PrintAsterisk().start();
+        new PrintEqual().start();
+    }
+}
+
+class PrintAsterisk extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("***");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                System.out.println("スレッドの割り込みが発生しました。");
+            }
+        }
+    }
+}
+
+class PrintEqual extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("=====");
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                System.out.println("スレッドの割り込みが発生しました。");
+            }
+        }
+    }
+}
